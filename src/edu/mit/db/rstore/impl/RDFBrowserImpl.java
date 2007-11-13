e3b4d714d7a47238d68f40138e4f679d8771dcef
oshanis <oshanis@bedc46cc-713e-0410-904d-7f450f53edee>
2007-11-13 22:21:51 +0000

    Implementation of RDF Browser

---
+++ src/edu/mit/db/rstore/impl/RDFBrowserImpl.java
@@ -0,0 +1,28 @@
+package edu.mit.db.rstore.impl;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.Hashtable;
+import java.util.LinkedList;
+
+import edu.mit.db.rstore.RDFBrowser;
+
+public class RDFBrowserImpl extends HttpServlet implements RDFBrowser{
+
+	public Hashtable runQuery(String query) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void displayData(LinkedList<String> data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void displaySchema(LinkedList<String> schemaStructure) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
