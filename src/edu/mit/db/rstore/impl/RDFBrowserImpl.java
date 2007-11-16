ad1aacaf6e7af9c2816b57b7acb3536540d99c13
oshanis <oshanis@bedc46cc-713e-0410-904d-7f450f53edee>
2007-11-16 00:29:08 +0000

    RDF Browser implementation skeleton

--- src/edu/mit/db/rstore/impl/RDFBrowserImpl.java
+++ src/edu/mit/db/rstore/impl/RDFBrowserImpl.java
@@ -22,7 +22,32 @@ public class RDFBrowserImpl extends HttpServlet implements RDFBrowser{
 
 	public void displaySchema(LinkedList<String> schemaStructure) {
 		// TODO Auto-generated method stub
-		
+	
 	}
+	
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<head>");
+        out.println("<title>RDF Browser</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h3>RDF Browser</h3>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+    
+    /**
+     * Just pass the work to the GET method
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws IOException, ServletException
+    {
+        doGet(request, response);
+    }
 
 }
