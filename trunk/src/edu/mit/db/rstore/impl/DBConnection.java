f15b875a225a3e96d312767af4d2b5fd70f4f681
oshanis <oshanis@bedc46cc-713e-0410-904d-7f450f53edee>
2007-12-04 21:06:18 +0000

    DBConnection class stub to connect to the postgres database

---
+++ trunk/src/edu/mit/db/rstore/impl/DBConnection.java
@@ -0,0 +1,43 @@
+package edu.mit.db.rstore.impl;
+
+import java.sql.*;
+
+import com.hp.hpl.jena.vocabulary.DB;
+
+/**
+ * This class creates Database connection to PostgresSQL 
+ * (Helper class for DBPopulator)
+ * @author oshani
+ *
+ */
+public class DBConnection {
+	
+	// FIXME Throws a run-time error complaining about the password about "postgres"
+	
+	public void connect() throws ClassNotFoundException, SQLException{
+		
+		Class.forName("org.postgresql.Driver");
+		String url = "jdbc:postgresql:rstoreDB";
+		String username = "postgres";
+		String password = "postgres";
+		
+		Connection conn = DriverManager.getConnection(url, username, password);
+		
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery("CREATE TABLE base (subject varchar(40)" +
+														 " predicate varchar(40)" +
+														 " object) varchar(40)");
+		while (rs.next()) {
+		    System.out.print("Column 1 returned ");
+		    System.out.println(rs.getString(1));
+		}
+		rs.close();
+		st.close();
+
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException{
+		DBConnection dbc = new DBConnection();
+		dbc.connect();
+	}
+}
