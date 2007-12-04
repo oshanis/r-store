c39771b2d6a09c4bf47f5c739bb7f77fda4bee95
oshanis <oshanis@bedc46cc-713e-0410-904d-7f450f53edee>
2007-12-04 21:18:37 +0000

    changes to the comments!

--- src/edu/mit/db/rstore/impl/DBConnection.java
+++ src/edu/mit/db/rstore/impl/DBConnection.java
@@ -12,21 +12,22 @@ import com.hp.hpl.jena.vocabulary.DB;
  */
 public class DBConnection {
 	
-	// FIXME Throws a run-time error complaining about the password about "postgres"
-	
 	public void connect() throws ClassNotFoundException, SQLException{
 		
 		Class.forName("org.postgresql.Driver");
 		String url = "jdbc:postgresql:rstoreDB";
+		
+		//Make sure that you have a user named postgres with the same password
+		//otherwise it will not work!
 		String username = "postgres";
 		String password = "postgres";
 		
 		Connection conn = DriverManager.getConnection(url, username, password);
 		
 		Statement st = conn.createStatement();
-		ResultSet rs = st.executeQuery("CREATE TABLE base (subject varchar(40)" +
-														 " predicate varchar(40)" +
-														 " object) varchar(40)");
+		ResultSet rs = st.executeQuery("CREATE TABLE base (subject varchar(40)," +
+														 " predicate varchar(40)," +
+														 " object varchar(40))");
 		while (rs.next()) {
 		    System.out.print("Column 1 returned ");
 		    System.out.println(rs.getString(1));
