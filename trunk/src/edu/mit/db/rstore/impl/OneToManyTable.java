48f0e40c4a41bcd8ce9db69904945884b0ff4162
oshanis <oshanis@bedc46cc-713e-0410-904d-7f450f53edee>
2007-12-10 00:08:45 +0000

    completed OneToManyTable

--- trunk/src/edu/mit/db/rstore/impl/OneToManyTable.java
+++ trunk/src/edu/mit/db/rstore/impl/OneToManyTable.java
@@ -1,5 +1,8 @@
 package edu.mit.db.rstore.impl;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * This class represents the tables which are members of a rdf:domain which is a class identified 
  * as a table in the RDB schema and a rdf:range identified as a collection entity such as rdf:Seq
@@ -16,9 +19,70 @@ public class OneToManyTable extends PropertyTable {
 	private String pkey2_col_name;
 	private String predicate_name;
 	
-	public OneToManyTable(String tname, String primary_key, String primary_key_name) {
+	public OneToManyTable(String tname, String primary_key, String primary_key_name, String primary_key2, String primary_key2_name, String pred) {
 		super(tname, primary_key, primary_key_name);
-		// TODO Auto-generated constructor stub
+		pkey2 = primary_key2;
+		pkey2_col_name = primary_key2_name;
+		predicate_name = pred;
+	}
+	/**
+	 * Needs to be overridden in this class.  Should be simpler than the superclass
+	 */
+	public void constructSQL()
+	{	
+		String columnSQLString = "";
+		if (predicates_to_columns.size() > 0){
+			for(String s : predicates_to_columns.values())
+				columnSQLString += s + "  varchar, ";
+		}
+		else{
+			Iterator<String> i = columns.keySet().iterator();
+			while (i.hasNext()){
+				String col_type = (String)i.next();
+				columnSQLString += columns.get(col_type) + "  varchar, ";
+			}
+		}
+		create_table_command = "CREATE TABLE " + table_name +
+								"( " + pkey_col_name + " varchar, " +
+									   pkey2_col_name  + " varchar, " +
+								       columnSQLString +
+								 " PRIMARY KEY( " + pkey_col_name + " , " + pkey2_col_name +								 
+								 "))";
+
+	}
+	
+	public LinkedList<String> getPrimaryKeys()
+	{
+		LinkedList<String> pkeys = new LinkedList<String>();
+		pkeys.add(pkey);
+		pkeys.add(pkey2);
+		
+		return pkeys;
+	}
+	
+	public LinkedList<String> getPrimaryKeyColumns()
+	{
+		LinkedList<String> pkey_names = new LinkedList<String>();
+		
+		pkey_names.add(pkey_col_name);
+		pkey_names.add(pkey2_col_name);
+		
+		return pkey_names;
+	}
+	
+	public String getPredicate(){
+		return this.predicate_name;
+	}
+	
+	public void print()
+	{
+
+		System.out.println(table_name);
+		System.out.println(pkey_col_name + "  " + pkey2_col_name + " (pkey)  ");
+
+		//Predicates to columns is not used in this class
+		
+		System.out.println();
 	}
 
 }
